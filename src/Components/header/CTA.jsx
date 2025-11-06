d9ec30a0c7b87d70dcc3960e38368e2cace5a8b1
Surya <suryapillutla007@gmail.com>
2025-11-06 23:24:31 +0530

    updated resume

--- src/Components/header/CTA.jsx
+++ src/Components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
 
   return (
     <div>
-        <a href='https://drive.google.com/file/d/1Qmh88r93Zy9Kys26IYBh0jg24u9G5Yeq/view?usp=sharing'  target="_blank"
+        <a href='https://drive.google.com/file/d/1sVwuEyu7-kbuqnzrAJxChuEZkEJXifVz/view?usp=sharing'  target="_blank"
            rel="noopener noreferrer" className='btn'>Download CV</a>
         <a href='#contact' className='btn-primary'>Let's Talk</a>
     </div>
